1f8549f0d29347fb54328f69caf569d34f7c3e14
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-05-07 16:43:53 +0200

    done

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -49,7 +49,7 @@ public class SimpleJDBCRepository {
 
     public User findUserById(Long userId) {
         try (Connection connection = CustomDataSource.getInstance().getConnection();
-             PreparedStatement ps = connection.prepareStatement(findUserByIdSQL, Statement.RETURN_GENERATED_KEYS)) {
+             PreparedStatement ps = connection.prepareStatement(findUserByIdSQL)) {
             ps.setLong(1, userId);
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
@@ -66,25 +66,24 @@ public class SimpleJDBCRepository {
 
     public User findUserByName(String userName) {
         try (Connection connection = CustomDataSource.getInstance().getConnection();
-             PreparedStatement ps = connection.prepareStatement(findUserByNameSQL, Statement.RETURN_GENERATED_KEYS)) {
+             PreparedStatement ps = connection.prepareStatement(findUserByNameSQL)) {
             ps.setString(1, userName);
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
                     return new User(rs.getLong("id"), rs.getString("firstName"), rs.getString("lastName"), rs.getInt("age"));
-                } else {
-                    return null;
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        // User not found in database
         return null;
     }
 
     public List<User> findAllUser() {
         List<User> list = new ArrayList<>();
         try (Connection connection = CustomDataSource.getInstance().getConnection();
-             PreparedStatement ps = connection.prepareStatement(findAllUserSQL, Statement.RETURN_GENERATED_KEYS)) {
+             PreparedStatement ps = connection.prepareStatement(findAllUserSQL)) {
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next()) {
                     list.add(new User(rs.getLong(1), rs.getString(2), rs.getString(3), rs.getInt(4)));
@@ -98,13 +97,15 @@ public class SimpleJDBCRepository {
 
     public User updateUser(User user) {
         try (Connection connection = CustomDataSource.getInstance().getConnection();
-             PreparedStatement ps = connection.prepareStatement(updateUserSQL, Statement.RETURN_GENERATED_KEYS)) {
+             PreparedStatement ps = connection.prepareStatement(updateUserSQL)) {
             ps.setString(1, user.getFirstName());
             ps.setString(2, user.getLastName());
             ps.setInt(3, user.getAge());
             ps.setLong(4, user.getId());
-            ps.executeUpdate();
-            return findUserById(user.getId());
+            int rowsUpdated = ps.executeUpdate();
+            if (rowsUpdated > 0) {
+                return user;
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -115,7 +116,10 @@ public class SimpleJDBCRepository {
         try (Connection connection = CustomDataSource.getInstance().getConnection();
              PreparedStatement ps = connection.prepareStatement(deleteUser)) {
             ps.setLong(1, userId);
-            ps.executeUpdate();
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new RuntimeException("Failed to delete user with id " + userId);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
